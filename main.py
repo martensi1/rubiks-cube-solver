45ff8edbcdf7f98dfa4d57de3c68e8bdb4a4fda0
Simon Mårtensson <simon@norditech.se>
2023-07-21 15:55:55 +0200

    Add numbering of found squares

--- main.py
+++ main.py
@@ -3,48 +3,79 @@ import cv2
 
 
 class RubiksColor:
-    def __init__(self, name, color_range, visualization_color):
-        self.name = name
+    def __init__(self, id, color_range, visualization_color):
+        self.id = id
         self.color_range = color_range
         self.visualization_color = visualization_color
 
-    name: str
+    id: str
     color_range: tuple
     visualization_color: tuple
 
 
+class FoundSquare:
+    def __init__(self, contour, color):
+        self.contour = contour
+        self.color = color
+        self.number = 0
+
+    contour: np.ndarray
+    color: RubiksColor
+    number: int
+
+
+class CubeOrientation:
+    def __init__(self, origin, cw_next, cw_prev):
+        self.origin = origin
+        self.cw_next = cw_next
+        self.cw_prev = cw_prev
+
+    origin: cv2.Mat
+    cw_next: cv2.Mat
+    cw_prev: cv2.Mat
+
+
 rubiks_colors = [
     RubiksColor(
-        name="red",
+        id="red",
         color_range=[(0, 100, 70), (10, 255, 255)],
         visualization_color=(0, 0, 255)
     ),
     RubiksColor(
-        name="green",
+        id="green",
         color_range=[(50, 100, 70), (100, 255, 255)],
         visualization_color=(0, 255, 0)
     ),
     RubiksColor(
-        name="blue",
+        id="blue",
         color_range=[(100, 100, 70), (130, 255, 255)],
         visualization_color=(255, 0, 0)
     ),
     RubiksColor(
-        name="yellow",
+        id="yellow",
         color_range=[(20, 100, 70), (40, 255, 255)],
         visualization_color=(0, 255, 255)
     ),
     RubiksColor(
-        name="orange",
+        id="orange",
         color_range=[(10, 100, 70), (20, 255, 255)],
         visualization_color=(0, 165, 255)
     ),
     RubiksColor(
-        name="white",
+        id="white",
         color_range=[(0, 0, 100), (180, 110, 255)],
         visualization_color=(255, 255, 255)
     ),
 ]
+
+
+def is_square(aspect_ratio):
+    """
+    Check if a rectangle is a square
+    :param aspect_ratio: The aspect ratio of the rectangle
+    :return: True if the rectangle is a square, False otherwise
+    """
+    return 0.8 < aspect_ratio < 1.2
 
 
 def rectify_contour(contour):
@@ -62,18 +93,18 @@ def rectify_contour(contour):
     return box, aspect_ratio
 
 
-def find_contours_with_color_range(image, min_color, max_color):
+def find_contours_with_color_range(image, color_range):
     """
     Find all contours in an image that are within a certain color range
     :param image: Image to find contours in
-    :param min_color: Lower bound of the color range
-    :param max_color: Upper bound of the color range
+    :param color_range: Color range to find contours in
     :return: A list of found contours
     """
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_color, upper_color = color_range
 
-    color_mask = cv2.inRange(hsv_image, min_color, max_color)
-    contours, hierarchy = cv2.findContours(color_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    color_mask = cv2.inRange(hsv_image, lower_color, upper_color)
+    contours, hierarchy = cv2.findContours(color_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     return contours
 
@@ -132,25 +163,53 @@ def find_square_contours(contours):
         # We found a square!!
         square_contours.append(contour)
 
-        # x1, y1, w, h = cv2.boundingRect(contour)
-        # cv2.putText(frame, str(len(contour)) + "/" + str(len(side_lengths)), (x1, y1 - 5), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
-
     return square_contours
 
 
-def number_found_squares(frame, found_squares):
-    # Get the total contour of all squares together
-    side_contour = cv2.convexHull(
-        np.concatenate(found_squares)
+def find_cube_contour(found_squares):
+    """
+    Based on the found squares, find the contour of the entire Rubik's Cube
+    :param found_squares: The found squares
+    :return: Contour of cube
+    """
+    # If we do not have 9 squares, we cannot have a cube
+    if len(found_squares) != 9:
+        return None
+
+    # Get the total contour of all squares together with
+    # the help of the convex hull algorithm
+    all_contours = []
+    for square in found_squares:
+        all_contours.append(square.contour)
+
+    cube_contour = cv2.convexHull(
+        np.concatenate(all_contours)
     )
 
-    # Convert found contour to a rectangle
-    side_rect_contour, _ = rectify_contour(side_contour)
-    cv2.drawContours(frame, [side_rect_contour], 0, (0, 0, 255), 2)
+    # Rectify the cube contour and check if it is a square
+    cube_contour, aspect_ratio = rectify_contour(cube_contour)
 
-    # Find the orientation (origin, clockwise next point, clockwise previous point) of the rectangle
-    y_sorted_contour = side_rect_contour[side_rect_contour[:, 1].argsort()]
-    x_sorted_contour = side_rect_contour[side_rect_contour[:, 0].argsort()]
+    if not is_square(aspect_ratio):
+        return None
+
+    return cube_contour
+
+
+def find_cube_orientation(cube_contour):
+    """
+    Decide the orientation of the cube depending on which side is pointing upwards the most
+    Three corners of the cube will be labeled as origin, clockwise next and clockwise previous:
+    o=origin
+    cn=clockwise next
+    cp=clockwise previous
+    o---cn
+    |   |
+    cp--.
+    :param cube_contour: The contour of the cube
+    :return: The orientation of the cube (as a CubeOrientation object)
+    """
+    y_sorted_contour = cube_contour[cube_contour[:, 1].argsort()]
+    x_sorted_contour = cube_contour[cube_contour[:, 0].argsort()]
 
     top_points = y_sorted_contour[:2]
     leftmost_points = x_sorted_contour[:2]
@@ -158,45 +217,69 @@ def number_found_squares(frame, found_squares):
     origin_point, clockwise_next_point = top_points[top_points[:, 0].argsort()]
     clockwise_previous_point = next(p for p in leftmost_points if not np.array_equal(p, origin_point))
 
-    cv2.circle(frame, tuple(origin_point), 5, (255, 0, 0), 2)
-    cv2.circle(frame, tuple(clockwise_next_point), 5, (0, 255, 0), 2)
-    cv2.circle(frame, tuple(clockwise_previous_point), 5, (0, 0, 255), 2)
+    orientation = CubeOrientation(
+        origin=origin_point,
+        cw_next=clockwise_next_point,
+        cw_prev=clockwise_previous_point
+    )
 
-    # Estimate square positions based on the orientation of the rectangle
-    row_k_value = (clockwise_next_point[1] - origin_point[1]) / (clockwise_next_point[0] - origin_point[0])
-    column_k_value = (clockwise_previous_point[1] - origin_point[1]) / (clockwise_previous_point[0] - origin_point[0])
+    return orientation
 
-    row_length = np.linalg.norm(clockwise_next_point - origin_point)
-    column_length = np.linalg.norm(clockwise_previous_point - origin_point)
 
-    for i in range(9):
-        x = i % 3 + 1
-        y = i // 3 + 1
+def number_found_squares(frame, orientation, found_squares):
+    # Calculate the side length and tilt of the cube
+    origin_point = orientation.origin
+    cw_next_point = orientation.cw_next
+    cw_prev_point = orientation.cw_prev
 
-        # Calculate square position relative to the Cube's coordinate system
-        cube_x = x * row_length / 3 - row_length / 6
-        cube_y = y * column_length / 3 - column_length / 6
+    cube_tilt_k_value = (cw_next_point[1] - origin_point[1]) / (cw_next_point[0] - origin_point[0])
+    side_length = np.linalg.norm(cw_next_point - origin_point)
 
-        # Calculate square position relative to the image's coordinate system
-        theta = np.arctan2(row_k_value, 1)
+    for i in range(3):
+        # Calculate column position x-wise relative to Cube's coordinate system
+        x = i + 1
+        cube_x = x * side_length / 3 - side_length / 6
 
-        x = cube_x * np.cos(theta)
-        y = cube_x * np.sin(theta)
+        # Calculate row start and stop position relative to image's coordinate system
+        theta = np.arctan2(cube_tilt_k_value, 1)
 
-        # theta = np.arctan2(column_k_value, 1)
+        x_diff = cube_x * np.cos(theta)
+        y_diff = cube_x * np.sin(theta)
 
-        # x += cube_y * np.cos(theta)
-        # y += cube_y * np.sin(theta)
+        x1 = int(origin_point[0] + x_diff)
+        y1 = int(origin_point[1] + y_diff)
 
-        actual_x = origin_point[0] + x
-        actual_y = origin_point[1] + y
+        x2 = int(cw_prev_point[0] + x_diff)
+        y2 = int(cw_prev_point[1] + y_diff)
 
-        actual_x2 = clockwise_previous_point[0] + x
-        actual_y2 = clockwise_previous_point[1] + y
+        cv2.line(frame, (x1, y1), (x2, y2), (121, 131, 248), 2)
 
-        cv2.line(frame, (int(actual_x), int(actual_y)), (int(actual_x2), int(actual_y2)), (0, 0, 0), 2)
+        # Go down the column and find the squares
+        searchable_squares = found_squares.copy()
+        search_step_size = int(side_length / 6)
 
-        # cv2.circle(frame, (int(actual_x), int(actual_y)), 5, (0, 0, 0), 2)
+        num_found_squares = 0
+
+        for search_y in range(y1, y2, search_step_size):
+            search_x = np.interp(search_y, [y1, y2], [x1, x2])
+            search_x = int(search_x)
+
+            cv2.circle(frame, (search_x, search_y), 1, (0, 0, 0), 2)
+
+            for j, square in enumerate(searchable_squares):
+                intersects_square = cv2.pointPolygonTest(square.contour, (search_x, search_y), False) >= 0
+
+                if intersects_square:
+                    square_number = i + 1 + 3 * num_found_squares
+                    found_squares[j].number = square_number
+
+                    num_found_squares += 1
+                    del searchable_squares[j]
+
+                    cv2.putText(
+                        frame, str(square_number), (search_x, search_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2
+                    )
+                    break
 
 
 def find_rubiks_side(frame):
@@ -208,36 +291,36 @@ def find_rubiks_side(frame):
     """
     found_squares = []
 
-    # Denoise image
-    de_noised_image = cv2.fastNlMeansDenoisingColored(frame, None, 10, 10, 7, 21)
-
     # Search for squares in the image for each color
     for rubiks_color in rubiks_colors:
-        min_color, max_color = rubiks_color.color_range
-        found_contours = find_contours_with_color_range(de_noised_image, min_color, max_color)
+        color_range = rubiks_color.color_range
+        visualization_color = rubiks_color.visualization_color
 
+        found_contours = find_contours_with_color_range(frame, color_range)
         square_contours = find_square_contours(found_contours)
-        cv2.drawContours(frame, square_contours, -1, rubiks_color.visualization_color, 3)
 
-        for i, square_contour in enumerate(square_contours):
-            x, y, w, h = cv2.boundingRect(square_contour)
+        cv2.drawContours(frame, square_contours, -1, visualization_color, 3)
 
-            inverted_color = (
-                255 - rubiks_color.visualization_color[0],
-                255 - rubiks_color.visualization_color[1],
-                255 - rubiks_color.visualization_color[2]
-            )
+        for square_contour in square_contours:
+            found_squares.append(FoundSquare(square_contour, rubiks_color))
 
-            cv2.putText(frame, str(i), (x, y - 5), cv2.FONT_HERSHEY_COMPLEX, 1.0, inverted_color)
+    # Find the contour of the entire Rubik's Cube
+    cube_contour = find_cube_contour(found_squares)
 
-        found_squares += square_contours
+    if cube_contour is None:
+        return
 
-    # If we did not find all 9 squares, return None
-    if len(found_squares) != 9:
-        return None
+    cv2.drawContours(frame, [cube_contour], 0, (99, 49, 222), 2)
+
+    # Find cube orientation
+    cube_orientation = find_cube_orientation(cube_contour)
+
+    cv2.circle(frame, tuple(cube_orientation.origin), 5, (255, 0, 0), 2)
+    cv2.circle(frame, tuple(cube_orientation.cw_next), 5, (0, 255, 0), 2)
+    cv2.circle(frame, tuple(cube_orientation.cw_prev), 5, (0, 0, 255), 2)
 
     # Number the squares
-    number_found_squares(frame, found_squares)
+    number_found_squares(frame, cube_orientation, found_squares)
 
     return True
 
@@ -260,11 +343,11 @@ def main():
     # Show video in a window
     while True:
         ret, frame = cap.read()
-        processed = process_frame(frame)
 
+        processed = process_frame(frame)
         cv2.imshow('frame', processed)
 
-        if cv2.waitKey(50) & 0xFF == ord('q'):
+        if cv2.waitKey(25) & 0xFF == ord('q'):
             break
 
 
